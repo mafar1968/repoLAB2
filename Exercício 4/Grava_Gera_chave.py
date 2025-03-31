b0230530222f136e829e08408968dfd120a4698c
EmperiuM <sergio_marcelino@outlook.com>
2025-03-31 10:19:26 +0100

    upload exer 4
    exercício 4

---
+++ Exercício 4/Grava_Gera_chave.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+# Gera uma nova chave Fernet
+key = Fernet.generate_key()
+
+# Nome do arquivo onde a chave será gravada
+file_name = 'chave_fernet.key'
+
+# Grava a chave no arquivo
+with open(file_name, 'wb') as key_file:
+    key_file.write(key)
+
+print(f'Chave gerada e gravada em {file_name}')
